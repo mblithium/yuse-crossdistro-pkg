6a9a99866272d3e48aac9176636b075772f9dcb4
Matheus Bastos <matheusmcy1@gmail.com>
2024-01-23 20:25:32 -0300

    install script more informative

--- install.sh
+++ install.sh
@@ -1,6 +1,42 @@
 #!/usr/bin/env bash
 # This script "installs" the yuse wrapper package on your system.
 
+areYouSure() {
+    clear
+    userprompt=""
+    printf "Do you want to install the yuse package wrapper on your system? [y/n]\nIt will be installed in /opt/yuse-crosspkg\n> "
+    read userprompt
+
+    installOpt() {
+        echo "Installing..."
+        copyFiles
+        setExectobin
+        checkInstalation
+    }
+
+    noOpt() {
+        echo "You choose not to install, the script will be closed."
+    }
+
+    case $userprompt in
+    "y")
+        installOpt
+        ;;
+    "yes")
+        installOpt
+        ;;
+    "n")
+        noOpt
+        ;;
+    "no")
+        noOpt
+        ;;
+    *)
+        echo "I didn't understand your answer, so I'll take it as no."
+        ;;
+    esac
+}
+
 copyFiles() {
     if [[ -d /opt/yuse-crosspkg ]]; then
         sudo rm -r /opt/yuse-crosspkg
@@ -23,9 +59,7 @@ checkInstalation() {
 
     # fake, don't trust me
     echo "Nice, it's been installed successfully, trust me."
-
+    printf "use: \"yuse about\" for more information\n"
 }
 
-copyFiles
-setExectobin
-checkInstalation
+areYouSure
